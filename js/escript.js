21f5a4ec2bc6bed3ecc26e8d8541f3fb57d01710
matias <matiasuriel18@hotmail.com>
2023-05-01 15:44:02 -0300

    section about end

--- js/escript.js
+++ js/escript.js
@@ -1,7 +1,7 @@
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.remove('active');
+    navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
@@ -10,7 +10,7 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     navbar.classList.remove('active');
-    searchForm.classList.remove('active');
+    searchForm.classList.toggle('active');
     cartItem.classList.remove('active');
 }
 
@@ -20,7 +20,7 @@ let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#cart-btn').onclick = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+    cartItem.classList.toggle('active');
 }
 
 window.onscroll = () => {
